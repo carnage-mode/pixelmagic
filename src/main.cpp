75df67f2d161e51807f4332c880d7b2f5ebb5adf
Beamanuel Tesfaye <beta-stage@tutanota.com>
2022-08-03 09:47:35 +0300

    First working version, grayscale filter

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <fstream>
 
 struct Pixel
 {
@@ -33,11 +34,124 @@ struct BITMAPINFOHEADER
 	std::uint32_t bmpImportantColorNum;
 };
 
+void grayscale (Pixel** pixelMatrix, std::int32_t height, std::int32_t width);
 
 int main()
 {
-	std::cout << "Hello World\n";
+	while (true)
+	{
+		std::cout << "Name of the image file (BMP format only): ";
+		std::string fileName {};
+		std::getline(std::cin >> std::ws, fileName);
+		std::fstream imageFile(fileName.c_str(), std::ios::in | std::ios::out | std::ios::binary);
+
+		if (imageFile.fail())
+		{
+			std::cerr << fileName <<" can't be found. Try again\n";
+			continue;
+		}
+		else
+			std::cout << "Found the file!\n";
+
+		BITMAPFILEID id;
+		imageFile.read((char*)(&id), sizeof(id));
+
+		if (id != 0x4D42)
+		{
+			std::cerr << "File type is not BMP!\n";
+			imageFile.close();
+			continue;
+		}
+
+		BITMAPFILEHEADER fileheader;
+		BITMAPINFOHEADER infoheader;
+
+		imageFile.read((char*)(&fileheader), sizeof(fileheader));
+		imageFile.read((char*)(&infoheader), sizeof(infoheader));
+
+		if (infoheader.bmpDIBSize != 40)
+		{
+			std::cerr << "Unsupported version of BMP\n";
+			imageFile.close();
+			continue;
+		}
+
+		if (infoheader.bmpBitsPerPixel != 24)
+		{
+			std::cerr << "Only 24 bit images supported\n";
+			imageFile.close();
+			continue;
+		}
+
+		if (infoheader.bmpCompression != 0)
+		{
+			std::cerr << "Compressed images are not supported\n";
+			imageFile.close();
+			continue;
+		}
+
+		Pixel** pixelMatrix {new Pixel*[infoheader.bmpHeight]};
+		for (int i{0}; i < infoheader.bmpHeight; ++i)
+			pixelMatrix[i] = new Pixel[infoheader.bmpWidth];
+
+		if (infoheader.bmpHeight < 0)
+			infoheader.bmpHeight = -infoheader.bmpHeight;
+
+		imageFile.seekg(fileheader.bmpPixelArrayOffset);
+
+		for (int row {0}; row < infoheader.bmpHeight; ++row)
+		{
+			for (int col {0}; col < infoheader.bmpWidth; ++col)
+			{
+				pixelMatrix[row][col].blue = imageFile.get();
+				pixelMatrix[row][col].green = imageFile.get();
+				pixelMatrix[row][col].red = imageFile.get();
+			}
+
+			imageFile.seekg(infoheader.bmpWidth % 4, std::ios::cur);
+		}
+
+		grayscale(pixelMatrix, infoheader.bmpHeight, infoheader.bmpWidth);
+		std::cout << "Adding grayscale\n";
+
+		imageFile.seekg(fileheader.bmpPixelArrayOffset);
+
+		for (int row {0}; row < infoheader.bmpHeight; ++row)
+		{
+			for (int col {0}; col < infoheader.bmpWidth; ++col)
+			{
+				imageFile.put(pixelMatrix[row][col].blue);
+				imageFile.put(pixelMatrix[row][col].green);
+				imageFile.put(pixelMatrix[row][col].red);
+			}
+
+			imageFile.seekg(infoheader.bmpWidth % 4, std::ios::cur);
+		}
+		std::cout << "Success\n";
+		imageFile.close();
+
+		for(int i {0}; i < infoheader.bmpHeight; ++i)
+			delete[] pixelMatrix[i];
+		delete[] pixelMatrix;
+
+		break;
+	}
 
 	return 0;
 }
 
+void grayscale (Pixel** pixelMatrix, std::int32_t height, std::int32_t width)
+{
+	for (int row {0}; row < height; ++row)
+	{
+		for (int col {0}; col < width; ++col)
+		{
+			int average {pixelMatrix[row][col].blue + pixelMatrix[row][col].green + pixelMatrix[row][col].red};
+			average /= 3;
+			pixelMatrix[row][col].blue = average;
+			pixelMatrix[row][col].green = average;
+			pixelMatrix[row][col].red = average;
+		}
+
+	}
+}
